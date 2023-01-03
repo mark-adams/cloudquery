15c6e83b316a87da163133ccbe158153e5801a62
candiduslynx <candiduslynx@gmail.com>
2023-01-03 15:48:32 +0200

    default transformers

--- plugins/destination/mssql/client/transform.go
+++ plugins/destination/mssql/client/transform.go
@@ -4,87 +4,100 @@ import (
 	"github.com/cloudquery/plugin-sdk/schema"
 )
 
-func (c *Client) TransformBool(b *schema.Bool) any {
-	//TODO implement me
-	panic("implement me")
+func (*Client) TransformBool(v *schema.Bool) any {
+	return v.Bool
 }
 
-func (c *Client) TransformBytea(bytea *schema.Bytea) any {
-	//TODO implement me
-	panic("implement me")
+func (c *Client) TransformBytea(v *schema.Bytea) any {
+	return v.Bytes
 }
 
-func (c *Client) TransformCIDRArray(array *schema.CIDRArray) any {
-	//TODO implement me
-	panic("implement me")
+func (c *Client) TransformCIDRArray(v *schema.CIDRArray) any {
+	res := make([]string, len(v.Elements))
+	for i, e := range v.Elements {
+		res[i] = e.String()
+	}
+	return res
 }
 
-func (c *Client) TransformCIDR(cidr *schema.CIDR) any {
-	//TODO implement me
-	panic("implement me")
+func (c *Client) TransformCIDR(v *schema.CIDR) any {
+	return v.String()
 }
 
-func (c *Client) TransformFloat8(float8 *schema.Float8) any {
-	//TODO implement me
-	panic("implement me")
+func (c *Client) TransformFloat8(v *schema.Float8) any {
+	return v.Float
 }
 
-func (c *Client) TransformInetArray(array *schema.InetArray) any {
-	//TODO implement me
-	panic("implement me")
+func (c *Client) TransformInetArray(v *schema.InetArray) any {
+	res := make([]string, len(v.Elements))
+	for i, e := range v.Elements {
+		res[i] = e.String()
+	}
+	return res
 }
 
-func (c *Client) TransformInet(inet *schema.Inet) any {
-	//TODO implement me
-	panic("implement me")
+func (c *Client) TransformInet(v *schema.Inet) any {
+	return v.String()
 }
 
-func (c *Client) TransformInt8Array(array *schema.Int8Array) any {
-	//TODO implement me
-	panic("implement me")
+func (c *Client) TransformInt8Array(v *schema.Int8Array) any {
+	res := make([]int64, len(v.Elements))
+	for i, e := range v.Elements {
+		res[i] = e.Int
+	}
+	return res
 }
 
-func (c *Client) TransformInt8(i *schema.Int8) any {
-	//TODO implement me
-	panic("implement me")
+func (c *Client) TransformInt8(v *schema.Int8) any {
+	return v.Int
 }
 
-func (c *Client) TransformJSON(json *schema.JSON) any {
-	//TODO implement me
-	panic("implement me")
+func (c *Client) TransformJSON(v *schema.JSON) any {
+	if v.Status != schema.Present {
+		return nil
+	}
+	return string(v.Bytes)
 }
 
-func (c *Client) TransformMacaddrArray(array *schema.MacaddrArray) any {
-	//TODO implement me
-	panic("implement me")
+func (c *Client) TransformMacaddrArray(v *schema.MacaddrArray) any {
+	res := make([]string, len(v.Elements))
+	for i, e := range v.Elements {
+		res[i] = e.String()
+	}
+	return res
 }
 
-func (c *Client) TransformMacaddr(macaddr *schema.Macaddr) any {
-	//TODO implement me
-	panic("implement me")
+func (c *Client) TransformMacaddr(v *schema.Macaddr) any {
+	return v.String()
 }
 
-func (c *Client) TransformTextArray(array *schema.TextArray) any {
-	//TODO implement me
-	panic("implement me")
+func (c *Client) TransformTextArray(v *schema.TextArray) any {
+	res := make([]string, len(v.Elements))
+	for i, e := range v.Elements {
+		res[i] = e.String()
+	}
+	return res
 }
 
-func (c *Client) TransformText(text *schema.Text) any {
-	//TODO implement me
-	panic("implement me")
+func (c *Client) TransformText(v *schema.Text) any {
+	return v.String()
 }
 
-func (c *Client) TransformTimestamptz(timestamptz *schema.Timestamptz) any {
-	//TODO implement me
-	panic("implement me")
+func (c *Client) TransformTimestamptz(v *schema.Timestamptz) any {
+	if v.Status != schema.Present {
+		return nil
+	}
+	return v.Time
 }
 
-func (c *Client) TransformUUIDArray(array *schema.UUIDArray) any {
-	//TODO implement me
-	panic("implement me")
+func (*Client) TransformUUIDArray(v *schema.UUIDArray) any {
+	res := make([]string, len(v.Elements))
+	for i, e := range v.Elements {
+		res[i] = e.String()
+	}
+	return res
 }
 
-func (c *Client) TransformUUID(uuid *schema.UUID) any {
-	//TODO implement me
-	panic("implement me")
+func (*Client) TransformUUID(v *schema.UUID) any {
+	return v.String()
 }
